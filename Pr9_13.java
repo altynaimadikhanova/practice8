33f0f97d71ffe389834eda395c7806407d953521
altynaimadikhanova <altynajmadihanova397@gmail.com>
2023-01-01 22:33:52 +0600

    first commit

---
+++ Pr9_13.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Pr9_13{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.nextLine();
+		char c0 = word.charAt(2);
+		System.out.println(c0);
+	}
+}
